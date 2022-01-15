f9ddba87af83fbf1d4a07337aab8b09e34ddbb52
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-15 17:14:00 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const messages = [];
+
 const names = ["Guadalupe", "Ollie", "Aki"];
 const event = "surprise";
 
-function writeCards(names){
-    for (let n = 0; n < names.length; i++ ){
-        console.log(`Thank you, ${names[n]}, for the wonderful birthday gift!`);
-    } return names
+function writeCards(names) {
+    const messages = [];
+    for (let n = 0; n < names.length; n++) {
+        messages.push(`Thank you, ${names[n]}, for the wonderful surprise gift!`);
+    } return messages;
+}
+
+function countDown() {
+    let n = 10;
+    while (n >= 0) {
+        console.log(n--);
+    }
 }
