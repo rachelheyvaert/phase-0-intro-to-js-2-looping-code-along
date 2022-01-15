4f27d9d6082ead287bd73db2eae55934e74dea8c
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-15 16:54:20 -0500

    pushing

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-// Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
+const messages = [];
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
 
-function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    }
-    return gifts;
-}
-wrapGifts(gifts);
-
-const senders = ["Charlie", "Rachel", "Ben"];
-
-function writeCards(senders) {
-    for (let s = 0; s < senders.length; s++) {
-    console.log(`Thank you, ${senders[s]}, for the wonderful birthday gift!`);
-    debugger;
-}
- 
-return senders;
-}
-writeCards(senders);
-
-const n = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-    function countDown() {
-        let countDown = 11;
-        while (countDown > 0) {
-            console.log(countDown--);
-        }
-countDown(10);
+function writeCards(names){
+    for (let n = 0; n < names.length; i++ ){
+        console.log(`Thank you, ${names[n]}, for the wonderful birthday gift!`);
+    } return names
 }
